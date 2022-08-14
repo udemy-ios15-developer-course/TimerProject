ea4acf8604af306ef7d6c9a13b7b28dbaa11ff8f
Chris Hand <cj.hand@gmail.com>
2022-08-14 12:34:42 -0700

    Initial Commit

---
+++ TimerProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TimerProject
+//
+//  Created by Chris Hand on 8/14/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
