b1b3c3824b7ed3ea613c7be7c0a721f529c70034
Chris Hand <cj.hand@gmail.com>
2022-08-14 15:03:28 -0700

    Making something a little more fancy

--- TimerProject/ViewController.swift
+++ TimerProject/ViewController.swift
@@ -8,31 +8,102 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    @IBOutlet weak var counter: UILabel!
+    private var timer = Timer()
+    private var count = 10
+    private var runState : RunState = .ready
     
-    var timer = Timer()
-    var count = 10
+    @IBOutlet weak var topButton: UIButton!
+    @IBOutlet weak var bottomButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        counter.text = "Counter: \(count)"
-        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(timerTicked), userInfo: nil, repeats: true)
+        setButtonState()
     }
+    
+    
+    private func setButtonState() {
+        switch runState {
+        case .ready:
+            topButton.setTitle("Start \(count)", for: .normal)
+            bottomButton.setTitle("increment + 10", for: .normal)
+         
+        case .running:
+            topButton.setTitle("pause: \(count)", for: .normal)
+            bottomButton.setTitle("increment + 10", for: .normal)
+            
+        case .paused:
+            topButton.setTitle("resume: \(count)", for: .normal)
+            bottomButton.setTitle("decrement - 10", for: .normal)
+            
+        case .completed:
+            topButton.setTitle("completed!", for: .normal)
+            bottomButton.setTitle("reset", for: .normal)
+            topButton.isEnabled = false
+        }
+    }
+    
     
     @objc
     private func timerTicked() {
         count -= 1
-        counter.text = "Counter: \(count)"
-        
-        if (count == 0) {
-            counter.text = "Times up!"
+        print("tick")
+        if (count <= 0) {
             timer.invalidate()
+            runState = .completed
         }
+        
+        setButtonState()
     }
     
-    @IBAction func startClicked(_ sender: Any) {
+    @IBAction func topButtonClicked(_ sender: Any) {
+        switch runState {
+        case .ready:
+            runState = .running
+            timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(timerTicked), userInfo: nil, repeats: true)
+            
+        case .running:
+            timer.invalidate()
+            runState = .paused
+            
+        case .paused:
+            timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(timerTicked), userInfo: nil, repeats: true)
+            runState = .running
+            
+        case .completed:
+            print("Should never get to topButtonClicked.completed")
+        }
+        
+        setButtonState()
+    }
     
+    @IBAction func bottomButtonClicked(_ sender: Any) {
+        switch runState {
+        case .ready:
+            count += 10
+            
+        case .running:
+            count += 10
+            
+        case .paused:
+            count -= 10
+            if (count < 0) {
+                count = 0
+            }
+            
+        case .completed:
+            count = 10
+            topButton.isEnabled = true
+            runState = .ready
+        }
+        
+        setButtonState()
+    }
+    
+    private enum RunState {
+        case ready
+        case running
+        case paused
+        case completed
     }
 }
 
